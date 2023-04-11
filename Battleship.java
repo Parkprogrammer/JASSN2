348d952985957c0a1309947a9dd29aac8b4ffd67
Parkprogrammer <jaheon555@g.skku.edu>
2023-04-12 01:25:37 +0900

    battleship and input was written

--- Battleship.java
+++ Battleship.java
@@ -6,46 +6,51 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Battleship {
-	public char[] Rowindex = {'A', 'B', 'C', 'D', 'E', 'F', 'I', 'J'};
+	public static char[] Rowindex = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 	
 	public static void main(String[] args) throws IOException {
 		String fulladdress = "C:\\Users\\user\\eclipse-workspace\\PA2\\src\\";
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("The number of bombs: ");
 		int Nnombs = scan.nextInt();
-		
-		System.out.print(" // Program mode:");
-		String programmode = scan.nextLine();
-		System.out.println();
-		
-		System.out.print("The name of the board input file: ");
-		String fileaddress = scan.nextLine();
-		System.out.println();
-		
+		String programmode = scan.next();
+		String fileaddress = scan.next();
+				
 		fulladdress = fulladdress+fileaddress;
-		
+				
 		InputStream fis = new FileInputStream(fulladdress);
 		InputStreamReader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
 		
-		System.out.println(br.readLine());
-		System.out.println(br.readLine());
-		System.out.println(br.readLine());
-		System.out.println(br.readLine());
-		System.out.println(br.readLine());
-		System.out.println(br.readLine());
-		System.out.println(br.readLine());
-		System.out.println(br.readLine());
-		System.out.println(br.readLine());
-		System.out.println(br.readLine());
+		char[][] inputMap = new char [10][10];
+		char[][] Board = new char [12][35];
+		inputMap = changetxt(br);
+		Board = outputMap(inputMap); 
 		
+		for(int i=0;i<12;i++) {
+			for(int j=0;j<35;j++)
+				System.out.print(Board[i][j]);
+			System.out.println();
+		}
 		
 	}
 	
-	// public char[][] changetxt()
+	public static char[][] changetxt(BufferedReader File) throws IOException{
+		char[][] inputMap = new char[10][10];
+		
+		for(int i=0;i<10;i++) {
+			String textline = File.readLine();
+			for(int j=0;j<textline.length();j++) {
+				inputMap[i][j] = textline.charAt(j);			
+			}
+			for(int j=textline.length();j<10;j++)
+				inputMap[i][j] = ' ';
+		}
+		
+		return inputMap;
+	}
 	
-	public char[][] outputMap(char[][] inputMap){
+	public static char[][] outputMap(char[][] inputMap){
 		char[][] returnMap = new char[12][35];
 		
 		for(int i=0;i<12;i++) {								// 공백초기화
@@ -59,25 +64,26 @@ public class Battleship {
 					if(j==0) {								// 행번호 입력
 						if(i==11) {							// 10예외처리
 							returnMap[i][j]='1';
-							returnMap[i][j]='0';
+							returnMap[i][j+1]='0';
+							continue;
 						}
-						returnMap[i][j]=(char)(i-1);
+						returnMap[i][j]=(char)(i-1+'0');
 					}
 					
 					if(j==4)
 						returnMap[i][j]='|';
 					
-					if(j>4 && j/3==0)
+					if(j>4 && j%3==0)
 						returnMap[i][j] = inputMap[i-2][j/3-2];
 					
 				}
 				
 				if(i==0) { 									// 첫두줄 작성
-					if(j%3==0&&j!=3)
+					if(j!=0 && j%3==0 && j!=3)
 						returnMap[i][j] = Rowindex[j/3-2];
 				}
 				if(i==1) {
-					if(j%3==0&&j!=3)
+					if(j!=0 && j%3==0 && j!=3)
 						returnMap[i][j] = '-';
 				}
 		}
